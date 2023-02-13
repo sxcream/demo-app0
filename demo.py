a53430d01158c0fc7a916741aabd3be3616b33a8
sxcream <sexycream.booking@gmail.com>
2023-02-13 17:26:49 +0100

    afficher le contenu de extension.txt dans demo.py

--- demo.py
+++ demo.py
@@ -5,3 +5,9 @@ print(r.status_code)
 
 a = "hello world !"
 print(a)
+
+
+#affiche le contenu du fichier extension.txt 
+with open("extension.txt", "r") as f:
+    contenu = f.read()
+print(contenu)
